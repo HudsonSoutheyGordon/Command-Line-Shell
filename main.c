2c223b47e2b95e7cc99162c1f400bbeb8211857a
HudsonSoutheyGordon <southeyh@oregonstate.edu>
2021-10-20 15:39:30 -0700

    main.c created and added

---
+++ main.c
@@ -0,0 +1,21 @@
+// If you are not compiling with the gcc option --std=gnu99, then
+// uncomment the following line or you might get a compiler warning
+//#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <time.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[])
+{
+    printf("\n\n ############## START OF PROGRAM ##############\n\n");
+
+    return EXIT_SUCCESS;
+}
